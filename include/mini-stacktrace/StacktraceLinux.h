9db7ed3c7c4fbbf7eca939adc55ade9cdad47578
Hifumi Shiroan <shiroan@qq.com>
2022-10-17 22:55:23 +0800

    Init

---
+++ include/mini-stacktrace/StacktraceLinux.h
@@ -0,0 +1,14 @@
+//
+// Created by shiroan on 22-10-17.
+//
+
+#ifndef MINI_STACKTRACE_STACKTRACELINUX_H
+#define MINI_STACKTRACE_STACKTRACELINUX_H
+
+namespace mini_stacktrace {
+    class StacktraceLinux {
+
+    };
+}
+
+#endif //MINI_STACKTRACE_STACKTRACELINUX_H
